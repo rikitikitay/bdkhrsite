ece3a54ed61daeaba8aa008dc3e89e2a31ed7a40
rikitikitay <lashkin2001@mail.ru>
2020-12-06 20:57:54 +0300

    made code more clear

--- www/khr/khr.php
+++ www/khr/khr.php
@@ -7,17 +7,20 @@
 <form  name="khr" action="edit_khr.php">
 <?php 
 	include("../connection.php");
-	$idkhr = $_GET["idkhr"];
-	echo '<label><input type="hidden" name="idkhr" value="'; echo $idkhr; echo '"</label>';
-	$query = "SELECT * FROM `khr` WHERE `id`='".$idkhr."'";
+    $idkhr = $_GET["idkhr"];
+?>    
+<label>
+<input type="hidden" name="idkhr" value="<?php echo $idkhr; ?>">
+</label>
+<?php
+    $query = "SELECT * FROM `khr` WHERE `id`='".$idkhr."'";
 	$res = mysql_query($query) or die(mysql_error());
-	$product=mysql_fetch_assoc($res); 
-	$number = $product['num'];
-	$title = $product['title'];
-	$order = $product['order'];
-    $begin = $product['begin'];
+	$row=mysql_fetch_array($res); 
+	$number = $row['num'];
+	$title = $row['title'];
+	$order = $row['order'];
+    $begin = $row['begin'];
 	mysql_close($dbh);
-	
 ?>
 <label>Номер<input type="text" name="number" size="10" value=<?php echo $number; ?> ></label><br>
 <label>Название<input type="text" name="title" size="100" value="<?php echo htmlspecialchars(stripslashes($title)); ?>" ></label><br>
