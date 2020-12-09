f28d248dd5ce23c4d23f1ed222a2be6d60e32cef
rikitikitay <lashkin2001@mail.ru>
2020-12-09 20:33:12 +0300

    finished with dependency in update part of work

--- www/work/make_work.php
+++ www/work/make_work.php
@@ -28,48 +28,59 @@ if ($idwork > 0) {
     $query = 'UPDATE `work` 
         SET 
             `khr_id`='.$idkhr.',
-            `num`='.$number.',
-            `type`='.$typework.',
+            `num`="'.$number.'",
+            `type`="'.$typework.'",
             `title`="'.$title.'",
             `begin_cur`="'.$begin_current.'",
             `end_cur`="'.$end_current.'" 
         WHERE `id`='.$idwork;
 echo $query.'<br>';
     mysql_query($query) or die(mysql_error());
-    $query = 'SELECT `id` FROM `work` WHERE `num`='.$prev.' AND `khr_id`='.$idkhr;
-    $res = mysql_query($query);
-echo $query.'<br>';
-    if (mysql_num_rows($res) != 0) {
-        $row = mysql_fetch_array($res);
-        $idprev = $row['id'];
-    } else $idprev= 0;
-    $query = 'SELECT `id` FROM `work` WHERE `num`='.$next.' AND `khr_id`='.$idkhr.'';
-    print $query.'<br>';
-    $res = mysql_query($query);    
-    if (mysql_num_rows($res) != 0) {
-        $row = mysql_fetch_array($res);
-        $idnext = $row['id'];
-    } else $idnext = 0;
+    // update dependency
     $query = 'SELECT `id` FROM `dependency` WHERE `work_id`="'.$idwork.'"';
     echo $query.'<br>';
     $res = mysql_query($query) or mysql_error();
-    if (mysql_num_rows($res) != 0)  {
+    $dep_id_count = mysql_num_rows($res);
+    if ($dep_id_count != 0) { 
         $row = mysql_fetch_array($res);
-        $id = $row['id'];
-        $query = 'UPDATE `dependency`
-            SET
-                `khr_id`='.$idkhr.',
-                `prev_id`='.$idprev.',
-                `next_id`='.$idnext.',
-                `type`="'.$type_dependency.'"
-            WHERE `id`='.$id;        echo $query.'<br>';    
+        $id_dep = $row['id'];       
+    } 
+    if ($dlt_dependency != 3) {
+        $query = 'SELECT `id` FROM `work` WHERE `num`='.$prev.' AND `khr_id`='.$idkhr;
+        $res = mysql_query($query);
+    echo $query.'<br>';
+        if (mysql_num_rows($res) != 0) {
+            $row = mysql_fetch_array($res);
+            $idprev = $row['id'];
+        } else $idprev= 0;
+        $query = 'SELECT `id` FROM `work` WHERE `num`='.$next.' AND `khr_id`='.$idkhr.'';
+        print $query.'<br>';
+        $res = mysql_query($query);    
+        if (mysql_num_rows($res) != 0) {
+            $row = mysql_fetch_array($res);
+            $idnext = $row['id'];
+        } else $idnext = 0;
+        if ($id_dep != 0)  {
+            $query = 'UPDATE `dependency`
+                SET
+                    `khr_id`='.$idkhr.',
+                    `prev_id`='.$idprev.',
+                    `next_id`='.$idnext.',
+                    `type`="'.$type_dependency.'"
+                WHERE `id`='.$id_dep;        echo $query.'<br>';
+        } else {
+            $query = 'INSERT INTO `dependency`(`id`,`khr_id`,`work_id`,`prev_id`,`next_id`,`type`) 
+                    VALUES (NULL,'.$idkhr.','.$idwork.','.$idprev.','.$idnext.','.$type_dependency.')';echo $query.'<br>';
+        } 
+        mysql_query($query) or die(mysql_error());
     } else {
-        $query = 'INSERT INTO `dependency`(`id`,`khr_id`,`work_id`,`prev_id`,`next_id`,`type`) 
-                VALUES (NULL,'.$idkhr.','.$idwork.','.$idprev.','.$idnext.','.$type_dependency.')';echo $query.'<br>';
-    };
-    mysql_query($query) or die(mysql_error());
-    
-    } else {
-    
-    }        
+        if ($id_dep != 0) {
+            $query = 'DELETE FROM `dependency` WHERE `id`='.$id_dep;
+            mysql_query($query);     
+        }
+    }
+} else 
+{
+}
+mysql_close($dbh);
 ?>
