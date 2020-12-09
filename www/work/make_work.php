1dcbfae0d3bbfec040830e849afafdb170eadfda
rikitikitay <lashkin2001@mail.ru>
2020-12-10 01:37:32 +0300

    finished with update part of work handler

--- www/work/make_work.php
+++ www/work/make_work.php
@@ -13,7 +13,6 @@ if (!$prev) {
     $prev = 'NULL';
     $dlt_dependency += 1;    
 }
-var_dump($prev);
 $next = mysql_real_escape_string($_GET['next']);
 if (!$next) {
     $next = 'NULL';
@@ -34,11 +33,9 @@ if ($idwork > 0) {
             `begin_cur`="'.$begin_current.'",
             `end_cur`="'.$end_current.'" 
         WHERE `id`='.$idwork;
-echo $query.'<br>';
     mysql_query($query) or die(mysql_error());
     // update dependency
     $query = 'SELECT `id` FROM `dependency` WHERE `work_id`="'.$idwork.'"';
-    echo $query.'<br>';
     $res = mysql_query($query) or mysql_error();
     $dep_id_count = mysql_num_rows($res);
     if ($dep_id_count != 0) { 
@@ -48,13 +45,11 @@ echo $query.'<br>';
     if ($dlt_dependency != 3) {
         $query = 'SELECT `id` FROM `work` WHERE `num`='.$prev.' AND `khr_id`='.$idkhr;
         $res = mysql_query($query);
-    echo $query.'<br>';
         if (mysql_num_rows($res) != 0) {
             $row = mysql_fetch_array($res);
             $idprev = $row['id'];
         } else $idprev= 0;
         $query = 'SELECT `id` FROM `work` WHERE `num`='.$next.' AND `khr_id`='.$idkhr.'';
-        print $query.'<br>';
         $res = mysql_query($query);    
         if (mysql_num_rows($res) != 0) {
             $row = mysql_fetch_array($res);
@@ -67,10 +62,10 @@ echo $query.'<br>';
                     `prev_id`='.$idprev.',
                     `next_id`='.$idnext.',
                     `type`="'.$type_dependency.'"
-                WHERE `id`='.$id_dep;        echo $query.'<br>';
+                WHERE `id`='.$id_dep;
         } else {
             $query = 'INSERT INTO `dependency`(`id`,`khr_id`,`work_id`,`prev_id`,`next_id`,`type`) 
-                    VALUES (NULL,'.$idkhr.','.$idwork.','.$idprev.','.$idnext.','.$type_dependency.')';echo $query.'<br>';
+                    VALUES (NULL,'.$idkhr.','.$idwork.','.$idprev.','.$idnext.','.$type_dependency.')';
         } 
         mysql_query($query) or die(mysql_error());
     } else {
@@ -79,6 +74,35 @@ echo $query.'<br>';
             mysql_query($query);     
         }
     }
+    //update volume
+    $query = 'SELECT MAX(`order`) AS `max_order` 
+            FROM `volume` 
+            WHERE `work_id`='.$idwork;
+    $res = mysql_query($query);
+    if (mysql_num_rows($res) != 0) {
+        $row = mysql_fetch_array($res);
+        $max_order = $row['max_order'];
+        $query = 'DELETE FROM `volume` WHERE `work_id`='.$idwork;
+        mysql_query($query);
+    }
+    for ($i = 1; $i <= 5 || $i <= $max_order + 1; ++$i) {
+        $resp = mysql_real_escape_string($_GET['resp'.$i]);
+        if ($resp == 'on') {
+            $resp = 1;
+        } else {
+            $resp = 0;
+        }
+        $executor_id = mysql_real_escape_string($_GET['exec'.$i]);
+        for ($j = 1; $j <= 4; ++$j) {
+            $volume = mysql_real_escape_string($_GET['order'.$i.'quoter'.$j]);
+            if (strlen($volume) != 0) {
+                $query = 'INSERT INTO `volume`(`id`,`khr_id`,`work_id`,`executor_id`,`quoter`,`volume`,`resp`,`order`)
+                        VALUES(NULL,'.$idkhr.','.$idwork.','.$executor_id.','.$j.','.$volume.','.$resp.','.$i.')';
+                mysql_query($query);
+            }
+        }        
+    } 
+    
 } else 
 {
 }
